40342fb49779589a7773c8897b67ee586dbdcb4c
kanishka dubey <dubeykanishka0@gmail.com>
2022-11-20 16:04:21 +0000

    Update Annotations.py

--- Annotations.py
+++ Annotations.py
@@ -10,15 +10,15 @@ def boundingBox(path):
         large = cv2.imread(filename, 1)
         small = cv2.cvtColor(large, cv2.COLOR_BGR2GRAY)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-        grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))  #bbox shape
+        grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)  #outline of the image
 
-        _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)  
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
-        connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1)) 
+        connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)  #Closing small gaps or holes  
 
-        contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) #finding white object from black background
 
         mask = np.zeros(bw.shape, dtype=np.uint8)
 
