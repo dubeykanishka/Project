fdf4cb27a7a86048ae418a238d556ef03897139e
kanishka dubey <dubeykanishka0@gmail.com>
2022-11-20 15:22:19 +0000

    Update Split.py

--- Split.py
+++ Split.py
@@ -4,9 +4,10 @@ import glob
 def split(imgs):
     count = 0
     for filename in glob.glob(imgs):
-        img = cv2.imread(filename, 1)
-        for r in range(0,img.shape[0],1000):
-            for c in range(0,img.shape[1],1000):
+        img = cv2.imread(filename, 1)  #returns image in numpy
+        ## Using 1 to read image in color mode
+        for r in range(0,img.shape[0],1000): #rows
+            for c in range(0,img.shape[1],1000): #columns
                 count +=1
                 cv2.imwrite(f"E:\\Kanishka\\Dataset\\Crop_fullData_WCT\\" + str(count) + ".png",img[r:r+1000, c:c+1000,:])
 
